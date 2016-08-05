fa6611331ef19d8a1ff390b86c3509a0ba361686
eao197 <eao197@gmail.com>
2016-08-05 17:57:41 +0300

    Добавлено еще одно демонстрационное приложение v5_monitor.

--- dev/build.rb
+++ dev/build.rb
@@ -3,26 +3,27 @@ require 'mxx_ru/cpp'
 
 MxxRu::Cpp::composite_target( MxxRu::BUILD_ROOT ) {
 
-	toolset.force_cpp14
-	global_include_path "."
+  toolset.force_cpp14
+  global_include_path "."
 
-	if 'gcc' == toolset.name || 'clang' == toolset.name
-		global_linker_option '-pthread'
-	end
+  if 'gcc' == toolset.name || 'clang' == toolset.name
+    global_linker_option '-pthread'
+  end
 
-	# If there is local options file then use it.
-	if FileTest.exist?( "local-build.rb" )
-		required_prj "local-build.rb"
-	else
-		default_runtime_mode( MxxRu::Cpp::RUNTIME_RELEASE )
-		MxxRu::enable_show_brief
-		global_obj_placement MxxRu::Cpp::RuntimeSubdirObjPlacement.new( 'target' )
-	end
+  # If there is local options file then use it.
+  if FileTest.exist?( "local-build.rb" )
+    required_prj "local-build.rb"
+  else
+    default_runtime_mode( MxxRu::Cpp::RUNTIME_RELEASE )
+    MxxRu::enable_show_brief
+    global_obj_placement MxxRu::Cpp::RuntimeSubdirObjPlacement.new( 'target' )
+  end
 
-	required_prj 'v1/prj.rb'
-	required_prj 'v2/prj.rb'
-	required_prj 'v3/prj.rb'
-	required_prj 'v4/prj.rb'
-	required_prj 'v5/prj.rb'
-	required_prj 'v6/prj.rb'
+  required_prj 'v1/prj.rb'
+  required_prj 'v2/prj.rb'
+  required_prj 'v3/prj.rb'
+  required_prj 'v4/prj.rb'
+  required_prj 'v5/prj.rb'
+  required_prj 'v5_monitor/prj.rb'
+  required_prj 'v6/prj.rb'
 }
---
+++ dev/v5_monitor/prj.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+require 'rubygems'
+
+gem 'Mxx_ru', '>= 1.3.0'
+
+require 'mxx_ru/cpp'
+
+MxxRu::Cpp::exe_target {
+
+  target 'v5_monitor_app'
+
+  required_prj 'so_5/prj_s.rb'
+
+  cpp_source 'main.cpp'
+}
+
